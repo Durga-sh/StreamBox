7d0d3408532527ed3b836da52552766f5fb9d39f
Durgashankar Puhan <puhandurgasankarp@gmail.com>
2025-05-07 22:21:14 +0530

    FIX

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@
 import dotenv from "dotenv"
 import mongoose from "mongoose";
 import express from "express"
-import userRouter from "./routes/user.routes.js";
+
 
 // import { DB_NAME } from "./constants.js";
 import connectDB from "./db/index.js";
@@ -28,7 +28,28 @@ connectDB()
 })
 
 
+import userRouter from "./routes/user.routes.js";
+import healthcheckRouter from "./routes/healthcheck.routes.js";
+import tweetRouter from "./routes/tweet.routes.js";
+import subscriptionRouter from "./routes/subscription.routes.js";
+import videoRouter from "./routes/video.routes.js";
+import commentRouter from "./routes/comment.routes.js";
+import likeRouter from "./routes/like.routes.js";
+import playlistRouter from "./routes/playlist.routes.js";
+import dashboardRouter from "./routes/dashboard.routes.js";
+
+//routes declaration
+app.use("/api/v1/healthcheck", healthcheckRouter);
 app.use("/api/v1/users", userRouter);
+app.use("/api/v1/tweets", tweetRouter);
+app.use("/api/v1/subscriptions", subscriptionRouter);
+app.use("/api/v1/videos", videoRouter);
+app.use("/api/v1/comments", commentRouter);
+app.use("/api/v1/likes", likeRouter);
+app.use("/api/v1/playlist", playlistRouter);
+app.use("/api/v1/dashboard", dashboardRouter);
+
+
 
 
 
